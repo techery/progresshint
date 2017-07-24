5d84304ac980d855a3d853797c98d97994c84be7
Aleksey Malevaniy <a.malevaniy@gmail.com>
2017-07-24 18:31:27 +0300

    Remove leak cannary

--- sample/src/main/java/com/example/progresshint/App.java
+++ sample/src/main/java/com/example/progresshint/App.java
@@ -1,12 +1,10 @@
 package com.example.progresshint;
 
 import android.app.Application;
-import com.squareup.leakcanary.LeakCanary;
 
 public class App extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
-    LeakCanary.install(this);
   }
 }
